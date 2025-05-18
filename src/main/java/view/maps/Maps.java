b808637aea8f4f131faace1ec94e59ab6114dd53
yailengarc <yailengarciap@gmail.com>
2025-05-18 16:04:45 -0400

    Agregar setLevel3 en Maps para nuevo mapas TMX

---
+++ src/main/java/view/maps/Maps.java
@@ -0,0 +1,22 @@
+package View.Maps;
+import com.almasb.fxgl.entity.level.Level;
+
+import static com.almasb.fxgl.dsl.FXGL.setLevelFromMap;
+
+//A partir de esta clase se van a crear los mapas de juego
+public class Maps {
+    public static void setLevel1()
+    {
+        Level level = setLevelFromMap("level_01.tmx");
+    }
+
+
+    public static void setLevel2 (){Level level = setLevelFromMap("level_02.tmx");}
+
+ public static void setLevel3() 
+    {
+         Level level = setLevelFromMap("level_03.tmx");
+    }
+
+
+}
