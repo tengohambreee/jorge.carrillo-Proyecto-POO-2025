4368191c15e9a686eb687ca744f1776e7a0e9add
yailengarc <yailengarciap@gmail.com>
2025-05-18 16:53:57 -0400

    Agregar manejador de colisión para transición automática entre niveles

--- src/main/java/App/Game/GameApp.java
+++ src/main/java/App/Game/GameApp.java
@@ -242,6 +242,12 @@ public class GameApp extends GameApplication
         collitionService.startCollitionItemLife(combatModeUI);
         collitionService.starDoorCollition(doorLevel1);
 
+        FXGL.getPhysicsWorld().addCollisionHandler(new CollisionHandler(Types.PLAYER, Types.DOOR) {
+         @Override
+         protected void onCollisionBegin(Entity player, Entity door) {
+           levelManager.nextLevel(); // Cambio automático de nivel
+         }
+       });
     }
 
     @Override
